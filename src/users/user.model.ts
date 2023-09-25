cc03b4f1d221afad6efc2f93a749e1b570f611de
Iryna Bibik <bibik.iryna004@gmail.com>
2023-09-25 12:15:19 +0300

    requests for getting all users and creating user && swagger documentation

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Column, DataType, Model, Table } from 'sequelize-typescript';
 
 interface UserCreationAttrs {
@@ -7,6 +8,7 @@ interface UserCreationAttrs {
 
 @Table({ tableName: 'users' })
 export class User extends Model<User, UserCreationAttrs> {
+    @ApiProperty({ example: '1', description: 'Unique id' })
     @Column({
         type: DataType.INTEGER,
         unique: true,
@@ -15,6 +17,7 @@ export class User extends Model<User, UserCreationAttrs> {
     })
     id: Number;
 
+    @ApiProperty({ example: 'user@gmail.com', description: 'Email' })
     @Column({
         type: DataType.STRING,
         unique: true,
@@ -22,18 +25,21 @@ export class User extends Model<User, UserCreationAttrs> {
     })
     email: string;
 
+    @ApiProperty({ example: 'qwerty123', description: 'Password' })
     @Column({
         type: DataType.STRING,
         allowNull: false,
     })
     password: string;
 
+    @ApiProperty({ example: 'true', description: 'Banned user or not' })
     @Column({
         type: DataType.BOOLEAN,
         defaultValue: false,
     })
     banned: boolean;
 
+    @ApiProperty({ example: 'For lie', description: 'Banned reason' })
     @Column({
         type: DataType.STRING,
         allowNull: true,
