68ffa87f7545dfed5674f359a7443589b6deb59e
Iryna Bibik <bibik.iryna004@gmail.com>
2023-09-29 12:40:36 +0300

    Endpoints for giving roles and banning user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { User } from './user.model';
 import { InjectModel } from '@nestjs/sequelize';
 import { CreateUserDto } from './dto/create-user.dto';
 import { RolesService } from 'src/roles/roles.service';
+import { AddRoleDto } from './dto/add-role.dto';
+import { BanUserDto } from './dto/ban-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -31,6 +33,30 @@ export class UsersService {
             where: { email },
             include: { all: true },
         });
+        return user;
+    }
+
+    async addRole(dto: AddRoleDto) {
+        const user = await this.userRepository.findByPk(dto.userId);
+        const role = await this.roleService.getRoleByValue(dto.value);
+        if (role && user) {
+            await user.$add('role', role.id);
+            return dto;
+        }
+        throw new HttpException(
+            "User or role isn't found",
+            HttpStatus.NOT_FOUND
+        );
+    }
+
+    async ban(dto: BanUserDto) {
+        const user = await this.userRepository.findByPk(dto.userId);
+        if (!user) {
+            throw new HttpException("User isn't found", HttpStatus.NOT_FOUND);
+        }
+        user.banned = true;
+        user.banReason = dto.banReason;
+        await user.save();
         return user;
     }
 }
